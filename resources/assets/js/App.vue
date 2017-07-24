3a71b4859df44406e88f03a1369ec7671548d2bf
aminuddinz <aminblass93@hotmail.com>
2017-07-24 21:35:46 +0800

    first commit

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container">
+		<div class="navbar">
+			<div class="navbar__brand">
+				<router-link to="/">Recipe Box</router-link>
+			</div>
+			<ul class="navbar__list">
+				<li class="navbar__item" >
+					<router-link to="/login">LOGIN</router-link>
+				</li>
+				<li class="navbar__item" >
+					<router-link to="/register">REGISTER</router-link>
+				</li>
+				<li class="navbar__item" >
+					<router-link to="/recipes/create">CREATE RECIPE</router-link>
+				</li>
+				<li class="navbar__item" >
+					<a @click.stop="logout">LOGOUT</a>
+				</li>
+			</ul>
+		</div>
+		<router-view></router-view>
+	</div>
+</template>
